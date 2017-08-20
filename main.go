077928c9134dd39aaba004c7662602463423e564
utk9 <utkarshlamba.2@gmail.com>
2017-08-19 23:01:45 -0400

    Get target and current directories

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"os"
+	"fmt"
+)
+
 func main() {
-	
+	if (len(os.Args) < 2) {
+		fmt.Println("Must provide absolute path to target file/directory")
+		os.Exit(1)
+	}
+
+	target := os.Args[1]
+
+	current, err := os.Getwd()
+
+	if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+	fmt.Println(target)
+	fmt.Println(current)
+
 }
